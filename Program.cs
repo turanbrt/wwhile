9c5dd70422f715389370e4e02c18ffc6bc135b14
turan <mturan.barut@gmail.com>
2023-10-23 23:49:57 +0300

    c#101-while-foreach

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace wwhile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1 den n e kadar( n dahil) olan sayıların ortalamsını while ile konsola yazdıran program
+        Console.WriteLine("bir sayı giriniz.");
+        int sayi=int.Parse(Console.ReadLine());
+        int i=1,toplam=0,ort;
+        while(i<=sayi){
+            toplam=toplam+i;
+            i++;
+        }
+        ort=toplam/sayi;
+        Console.WriteLine("Ortalama:"+ort);
+
+
+        //A dan Z ye kadar olan sayıları yazdır
+
+        char character='A';
+        while(character<='Z'){
+            Console.WriteLine(character);
+            character ++;
+
+        }
+        //for each
+
+        string[] cars={"AUIDI","BMW","VOLKSWOGAN"};
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+
+    }
+}
